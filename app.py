17eb21c0fc0f074fb74bd68eba0f3e0fe1ca875d
Jady Godoi <jady.godoi@oi.net.br>
2026-07-13 16:10:12 -0300

    feat: novo endpoint para ex-2, schema e testes

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Olá mundo!"}
+
+
+@app.get("/exercicio-html", response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+        <head>
+            <title>Exercício Aula 02</title>
+        </head>
+        <body>
+            <h1>Exercício Aula 02</h1>
+            <p>Este é um exemplo de resposta HTML.</p>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,8 +6,17 @@ from app import app
 
 
 def test_root_dev_retornar_ok_e_ola_mundo():
-    client = TestClient(app) #Arrange
-    response = client.get("/") #Act
+    client = TestClient(app)  # Arrange
+    response = client.get("/")  # Act
 
-    assert response.status_code == HTTPStatus.OK #Assert
-    assert response.json() == {"message": "Olá mundo!"} #Assert
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {"message": "Olá mundo!"}  # Assert
+
+
+def test_exercicio_aula_02_retornar_html():
+    client = TestClient(app)  # Arrange
+    response = client.get("/exercicio-html")  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Asserts
+    assert "<h1>Exercício Aula 02</h1>" in response.text
+    assert "<p>Este é um exemplo de resposta HTML.</p>" in response.text
