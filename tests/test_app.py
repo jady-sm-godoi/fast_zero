ffcf6611784a95c368cb341eac77cba20414d93d
Jady Godoi <jady.godoi@oi.net.br>
2026-07-13 15:03:32 -0300

    chore: configuração inicial do projeto

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from app import app
+
+
+def test_root_dev_retornar_ok_e_ola_mundo():
+    client = TestClient(app) #Arrange
+    response = client.get("/") #Act
+
+    assert response.status_code == HTTPStatus.OK #Assert
+    assert response.json() == {"message": "Olá mundo!"} #Assert
