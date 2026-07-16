198e2688b6cc6d14e6abe9e8edf092c77d4cd9c2
Jady Godoi <jady.godoi@oi.net.br>
2026-07-16 13:53:21 -0300

    feat: implementa rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,84 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from app import app
+def test_create_user(client):
+    user = {
+        "username": "alice",
+        "email": "alice@example.com",
+        "password": "secret",
+    }
+    response = client.post("/users/", json=user)
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "alice",
+        "email": "alice@example.com",
+        "id": 1,
+    }
 
 
-def test_root_dev_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange
-    response = client.get("/")  # Act
+def test_create_user_with_invalid_email(client):
+    user = {
+        "username": "alice",
+        "email": "aliceexamplecom",
+        "password": "secret",
+    }
+    response = client.post("/users/", json=user)
 
-    assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {"message": "Olá mundo!"}  # Assert
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+    assert "value is not a valid email address" in response.text
 
 
-def test_exercicio_aula_02_retornar_html():
-    client = TestClient(app)  # Arrange
-    response = client.get("/exercicio-html")  # Act
+def test_read_users(client):
+    user = {
+        "username": "alice",
+        "email": "alice@example.com",
+        "password": "secret",
+    }
+    client.post("/users/", json=user)
 
-    assert response.status_code == HTTPStatus.OK  # Asserts
-    assert "<h1>Exercício Aula 02</h1>" in response.text
-    assert "<p>Este é um exemplo de resposta HTML.</p>" in response.text
+    response = client.get("/users/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "username": "alice",
+                "email": "alice@example.com",
+                "id": 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    user = {
+        "username": "alice",
+        "email": "alice@example.com",
+        "password": "secret",
+    }
+    client.post("/users/", json=user)
+
+    user_to_update = {
+        "username": "alice no pais das maravilhas",
+        "email": "alice@example.com",
+        "password": "outropassword",
+    }
+    response = client.put("/users/1", json=user_to_update)
+
+    assert response.status_code == HTTPStatus.OK
+    assert "alice no pais das maravilhas" in response.text
+
+
+def test_delete_user(client):
+    user = {
+        "username": "alice",
+        "email": "alice@example.com",
+        "password": "secret",
+    }
+    client.post("/users/", json=user)
+
+    response = client.delete("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "User deleted successfully"}
